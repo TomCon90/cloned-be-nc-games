73705cb88d57e1799ddc2612e8562f98b4e477d7
Tom Connor <tomconnor@Toms-MacBook-Air.local>
2021-11-01 14:35:33 +0000

    data inserted in to the relevant tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,5 @@
 const db = require("../../db");
-// const format = require("pg-format");
+const format = require("pg-format");
 
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
@@ -55,6 +55,65 @@ const seed = (data) => {
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       body VARCHAR NOT NULL
   );`);
+    })
+    .then(() => {
+      const queryStr = format(
+        `INSERT INTO categories
+  (slug, description)
+  VALUES
+  %L
+  RETURNING *;`,
+        categoryData.map((item) => [item.slug, item.description])
+      );
+      return db.query(queryStr);
+    })
+    .then(() => {
+      const queryStr = format(
+        `INSERT INTO users
+      (username, name, avatar_url)
+      VALUES
+      %L
+      RETURNING *;`,
+        userData.map((item) => [item.username, item.name, item.avatar_url])
+      );
+      return db.query(queryStr);
+    })
+    .then(() => {
+      const queryStr = format(
+        `INSERT INTO reviews
+      (title, designer, owner, review_img_url, review_body, category, created_at, votes)
+      VALUES
+      %L
+      RETURNING *;`,
+        reviewData.map((item) => [
+          item.title,
+          item.designer,
+          item.owner,
+          item.review_img_url,
+          item.review_body,
+          item.category,
+          item.created_at,
+          item.votes,
+        ])
+      );
+      return db.query(queryStr);
+    })
+    .then(() => {
+      const queryStr = format(
+        `INSERT INTO comments
+      (body, votes, author, review_id, created_at)
+      VALUES
+      %L
+      RETURNING *;`,
+        commentData.map((item) => [
+          item.body,
+          item.votes,
+          item.author,
+          item.review_id,
+          item.created_at,
+        ])
+      );
+      return db.query(queryStr);
     });
 
   // 2. insert data
