0248c9c06e72a32d3de0cdd3738b719fcd2394da
Tom Connor <tomconnor@Toms-MacBook-Air.local>
2021-11-04 12:41:29 +0000

    change to port to test set up

--- listen.js
+++ listen.js
@@ -1,5 +1,12 @@
 const app = require("./app");
 
-app.listen("9090", (req, res) => {
-  console.log("listening on 9090");
+// app.listen("9090", (req, res) => {
+//   console.log("listening on 9090");
+// });
+
+const { PORT } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on ${PORT}...`);
 });
