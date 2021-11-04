d3a38d8981479e84f33cc2a6a41e6c9af6cfe188
Tom Connor <tomconnor@Toms-MacBook-Air.local>
2021-11-04 14:42:54 +0000

    changes to get api JSON endpoint to ensure any further script leads to a URL not found.

--- routes/api.router.js
+++ routes/api.router.js
@@ -4,9 +4,9 @@ const reviewsRouter = require("./reviews.router");
 const commentsRouter = require("./comments.router");
 const { getAllEndpoints } = require("../controllers/api.controllers.js");
 
-apiRouter.use("/", getAllEndpoints);
 apiRouter.use("/reviews", reviewsRouter);
 apiRouter.use("/categories", categoriesRouter);
 apiRouter.use("/comments", commentsRouter);
+apiRouter.use("//", getAllEndpoints);
 
 module.exports = apiRouter;
