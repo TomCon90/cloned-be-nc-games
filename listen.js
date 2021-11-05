f7b565e0af99d2ccc70c348f7d9ad3051cffcc92
Tom Connor <tomconnor@Toms-MacBook-Air.local>
2021-11-05 13:09:08 +0000

    tidying up existing work based on feedback

--- listen.js
+++ listen.js
@@ -1,15 +1,9 @@
 const app = require("./app");
 
-// app.listen("9090", (req, res) => {
-//   console.log("listening on 9090");
-// });
-
 let { PORT } = process.env;
-console.log(PORT);
 if (PORT === undefined) {
   PORT = 9090;
 }
 app.listen(PORT, (err) => {
   if (err) throw err;
-  console.log(`Listening on ${PORT}...`);
 });
