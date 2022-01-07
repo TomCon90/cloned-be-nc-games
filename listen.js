3ebe78080201f0ed90b49205f28391e1f714884a
Tom Connor <tomconnor1990@gmail.com>
2022-01-07 12:06:49 +0000

    fixing endpoint for/api

--- listen.js
+++ listen.js
@@ -1,9 +1,10 @@
 const app = require("./app");
 
-let { PORT } = process.env;
+let { PORT } = process.env.PORT || 8000;
 if (PORT === undefined) {
   PORT = 9090;
 }
-app.listen(PORT || 5500, (err) => {
+app.listen(PORT, (err) => {
+  console.log("App is running on port ", PORT);
   if (err) throw err;
 });
